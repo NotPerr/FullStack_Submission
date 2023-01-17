5d9e84830b981dc555356f1d560a4668f8d2cf58
NotPerr <83740861+NotPerr@users.noreply.github.com>
2023-01-17 14:53:07 +0800

    exercise 2.12 complete

---
+++ part2/data-for-countries/src/App.js
@@ -0,0 +1,99 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+function App() {
+
+  // ---------------- fetch countries data ---------------- 
+  const [list,setList] = useState([])
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('https://restcountries.com/v3.1/all')
+      .then(response => {
+        console.log('promise fulfilled')
+        setList(response.data)
+      })
+  }, [])
+  // ---------------- set search value ---------------- 
+  const [search,setSearch] = useState('')
+  const [hasSearch,setHasSearch] = useState(false)
+
+  const handleSearchChange = (event) => {
+    setSearch(event.target.value)
+    console.log(search)
+    let filter = search
+    if(!filter) {
+      
+      setHasSearch(true)
+    }else {
+      setHasSearch(false)
+    }
+    console.log('has search:',hasSearch)
+  }
+  const addSearch = (event) => {
+    event.preventDefault()
+  }
+  // ---------------- set show list ---------------- 
+
+  const List = () => {
+    let filter = search.toLowerCase()
+    const setShowList = () => {
+      if(filter)
+      {
+        const newList = list.filter((country) => country.name.common.toLowerCase().includes(filter))
+        //console.log(newList)
+        return newList
+      }else {
+        return []
+      }
+      
+    }
+    const showList = setShowList()
+    console.log('show list: ',showList)
+
+    if(showList.length > 10)
+    {
+      return(<p>Too many matches, specify another filter</p>)
+    }else if(showList.length <=10 && showList.length > 1){
+      return (
+        showList.map(country => (
+          <p key={country.name.common}>{country.name.common}</p>
+        ))
+      )
+    }else if(showList.length === 1)
+    {
+      //console.log('common name: ',showList[0].name)
+      //console.log('languages: ',showList[0].languages)
+      const languages = Object.keys(showList[0].languages)
+      //console.log(languages)
+      return (
+        <>
+          <h1>{showList[0].name.common}</h1>
+          <p>capital: {showList[0].capital}</p>
+          <p>area: {showList[0].area}</p>
+          <h2>languages:</h2>
+          <ul>
+            {languages.map(lan => <li key={lan}>{showList[0].languages[lan]}</li>)}
+          </ul>
+          <img src={showList[0].flags.png}/>
+        </>
+      )
+    }
+    
+  }
+
+
+  return (
+    <div>
+      <form onSubmit={addSearch}>
+        <div>
+          <br />
+          find countries: <input value={search} onChange={handleSearchChange}/>
+        </div>
+      </form>
+      <List />
+    </div>
+  );
+}
+
+export default App;
